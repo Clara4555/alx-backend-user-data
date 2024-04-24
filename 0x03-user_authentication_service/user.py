70e9b01943ed4618aa34c286ab846a7a36e3ad5c
Clara4555 <favourloyalty8@gmail.com>
2024-04-24 12:44:32 -0700

    task

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-"""user.py.
+"""The `user` model's module.
 """
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
@@ -11,7 +11,7 @@ Base = declarative_base()
 class User(Base):
     """Represents a record from the `user` table.
     """
-    __table__ = "users"
+    __tablename__ = "users"
     id = Column(Integer, primary_key=True)
     email = Column(String(250), nullable=False)
     hashed_password = Column(String(250), nullable=False)
